322c41b8a5aca9f4ccc2b7aa8753062c2b8eeeca
VuralBayrakli <vuralll360@gmail.com>
2023-05-21 22:44:41 +0300

    guncellendi

--- grup_14/grup_14/settings.py
+++ grup_14/grup_14/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'grup_14.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dockerdb',
+        'USER': 'postgres',
+        'PASSWORD': 'Vural',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
