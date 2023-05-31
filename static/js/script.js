9aaec8a4460cc9cfdcb9df9bdfaf7e5d01dc6303
begüm <eminebegumaykac@gmail.com>
2023-05-31 19:46:21 +0300

    takip et butonu düzenlendi

--- static/js/script.js
+++ static/js/script.js
@@ -20,6 +20,21 @@ $(document).ready(function() {
       }
   }
 
+  //Takip et butonu
+
+  function followUser() {
+    var btn = document.getElementById("follow-btn");
+    if (btn.classList.contains("following")) {
+      btn.innerHTML = '<i class="fa-solid fa-xs fa-user-plus" style="color: #090606;"></i>';
+      btn.classList.remove("following");
+      btn.classList.add("follow-button");
+    } else {
+      btn.innerHTML = '<i class="fa-solid fa-xs fa-user-check" style="color: #1b6407;"></i>';
+      btn.classList.remove("follow-button");
+      btn.classList.add("following");
+    }
+  }
+  
 // Hakkında düzenle
 
 $(document).ready(function() {
